15e0ff7d6466a1601c8d1fda39decfbf3de647bd
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-12 14:31:43 -0300

    :tada: Commit inicial | Base Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Mobile</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5C6BC0',
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
