c83a028a6c9d1eb63bdbda55aa061a353325d632
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-14 12:50:29 -0300

    :sparkles: feat: Componente de slide dos Games

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,9 @@
-import { View, Image } from "react-native";
+import { View, Image, FlatList } from "react-native";
 import Logo from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+
+import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
@@ -12,6 +15,15 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => <GameCard data={item} />}
       />
     </View>
   );
