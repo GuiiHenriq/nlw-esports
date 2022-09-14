1cab26472117e0c0f6c824e6b9f2a04fabe3a9e5
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-13 22:47:50 -0300

    :heavy_plus_sign: build: Configurar TailwindCSS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,12 @@
+import './styles/main.css'
+import Logo from './assets/logo.svg'
+
 function App() {
-  return <h1>Web</h1>;
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center m-20'>
+      <img src={Logo} alt="Logo NLW eSports" />
+    </div>
+  );
 }
 
 export default App;
