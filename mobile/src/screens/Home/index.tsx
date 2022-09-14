f32a5763bb514788e05baaadd1606a3eab72fa00
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-14 12:18:59 -0300

    :sparkles: feat: Componente Header

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,18 @@
-import { View } from 'react-native';
- 
- import { styles } from './styles';
- 
- export function Home() {
-   return (
-     <View style={styles.container}>
- 
-     </View>
-   );
- }
+import { View, Image } from "react-native";
+import Logo from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
+}
