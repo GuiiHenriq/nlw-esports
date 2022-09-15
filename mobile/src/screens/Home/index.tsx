2eba66d096a8c61ed4a30975dcb522d7e8d6cb73
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-15 17:49:38 -0300

    :sparkles: feat: Integrar API e listar slide de Games no Mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,20 @@
 import { View, Image, FlatList } from "react-native";
+import { useState, useEffect } from "react";
 import Logo from "../../assets/logo-nlw-esports.png";
 import { Heading } from "../../components/Heading";
-import { GameCard } from "../../components/GameCard";
-
-import { GAMES } from "../../utils/games";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://192.168.1.6/games")
+      .then((res) => res.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.logo} />
@@ -19,7 +26,7 @@ export function Home() {
 
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         horizontal
         showsHorizontalScrollIndicator={false}
