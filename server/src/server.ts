a482356b32b415124bdb1fc51f299dd5712bd92e
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-14 22:31:17 -0300

    :sparkles: feat: Rota listagem Games

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,38 @@
-import express from 'express';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 // List Game
-app.get('/games', (req, res) => {
-  return res.json([]);
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return res.json(games);
 });
 
 // Create AD
-app.post('/ads', (req, res) => {
+app.post("/ads", (req, res) => {
   return res.json([]);
 });
 
 // List Ads by Game
-app.get('/games/:id/ads', (req, res) => {
+app.get("/games/:id/ads", (req, res) => {
   //const gameId = req.params.id;
 
   return res.json([]);
 });
 
 // Get Discord by Ad
-app.get('/ads/:id/discord', (req, res) => {
+app.get("/ads/:id/discord", (req, res) => {
   //const adId = req.params.id;
 
   return res.json([]);
