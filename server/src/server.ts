a51f010125ded1cfb2c3384003c02d9041809021
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-09-14 21:41:04 -0300

    :sparkles: feat: Definir casos de uso da API

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,21 +2,28 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
-  return res.json([
-    {
-      id: 1,
-      name: 'AD #1',
-    },
-    {
-      id: 2,
-      name: 'AD #2',
-    },
-    {
-      id: 3,
-      name: 'AD #3',
-    },
-  ]);
+// List Game
+app.get('/games', (req, res) => {
+  return res.json([]);
+});
+
+// Create AD
+app.post('/ads', (req, res) => {
+  return res.json([]);
+});
+
+// List Ads by Game
+app.get('/games/:id/ads', (req, res) => {
+  //const gameId = req.params.id;
+
+  return res.json([]);
+});
+
+// Get Discord by Ad
+app.get('/ads/:id/discord', (req, res) => {
+  //const adId = req.params.id;
+
+  return res.json([]);
 });
 
 app.listen(8888);
